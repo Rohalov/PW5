bdd6e1acaea0cc58975ea07781824db089ad341c
Rudic <rohalov777@gmail.com>
2024-04-03 10:17:15 +0300

    initial

---
+++ task1/Program.cs
@@ -0,0 +1,40 @@
+﻿
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var userInput = GetUserInput();
+
+        if (isPrime(userInput))
+        {
+            Console.WriteLine($"Число {userInput} є простим");
+        }
+        else
+        {
+            Console.WriteLine($"Число {userInput} не є простим");
+        }
+
+        Console.ReadKey();
+    }
+
+    private static bool isPrime(double number)
+    {
+        List<double> dividers = new List<double>();
+        for (int i = 1; i <= number; i++)
+        {
+            var div = number / i;
+            if (Math.Round(div, 0) == div)
+            {
+                dividers.Add(div);
+            }
+        }
+        return dividers.Count == 2;
+    }
+
+    private static double GetUserInput()
+    {
+        Console.WriteLine("Введіть число");
+        var result = Convert.ToDouble(Console.ReadLine());
+        return result;
+    }
+}
---
+++ task2/Program.cs
@@ -0,0 +1,43 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var userInput = GetUserInput();
+
+        ShowFibonachiNumbers(userInput);
+
+        Console.ReadKey();
+    }
+
+    private static void ShowFibonachiNumbers(int userInput)
+    {
+        List<int> fibonachiNumbers = GetFibLessNum(userInput);
+        Console.WriteLine("Числа Фібоначі");
+        foreach (int number in fibonachiNumbers)
+        {
+            Console.Write(number);
+            Console.Write(new string(' ', 3));
+        }
+    }
+
+    private static List<int> GetFibLessNum(int number)
+    {
+        List<int> fibnums = new List<int> { 0 };
+        int currNum = 1;
+        int i = 1;
+        while (currNum <= number)
+        {
+            fibnums.Add(currNum);
+            currNum = fibnums[i] + fibnums[i - 1];
+            i++;
+        }
+        return fibnums;
+    }
+
+    private static int GetUserInput()
+    {
+        Console.WriteLine("Введіть число");
+        var result = Convert.ToInt32(Console.ReadLine());
+        return result;
+    }
+}
